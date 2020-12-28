e0beddebe25fe903c7fb3ddfec69b2e02135a7af
Unufolio <unufolio@gmail.com>
2020-12-28 22:04:39 +0800

    init

---
+++ src/main/java/com/unufolio/demos/spring/rest/SpringRestSampleApplication.java
@@ -0,0 +1,13 @@
+package com.unufolio.demos.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unufolio/demos/spring/rest/SpringRestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unufolio.demos.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
